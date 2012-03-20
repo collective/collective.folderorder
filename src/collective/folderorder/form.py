7ece3e90ba8aed01979e9655313cb8d230d4d359
Jens W. Klein <jens@bluedynamics.com>
2012-03-20 19:55:32 +0100

    import yafowil.loader is fine

--- src/collective/folderorder/form.py
+++ src/collective/folderorder/form.py
@@ -1,5 +1,5 @@
 from zope.component import getAdapters
-import yafowil.plone
+import yafowil.loader
 from yafowil.controller import Controller
 from yafowil.yaml import parse_from_YAML
 from zope.i18nmessageid import MessageFactory
